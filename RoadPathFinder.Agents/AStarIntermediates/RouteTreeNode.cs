2e3f48c3465aebb668b274978f99ead1302d812e
Woo Sungyub <lumitrail@gmail.com>
2024-11-26 18:39:44 +0900

    RouteTreeNode

--- RoadPathFinder.Agents/AStarIntermediates/RouteTreeNode.cs
+++ RoadPathFinder.Agents/AStarIntermediates/RouteTreeNode.cs
@@ -1,14 +1,24 @@
-﻿using RoadPathFinder.Models.Elements;
+﻿using SmallGeometry.Euclidean;
+
+using RoadPathFinder.Models.Elements;
 
 namespace RoadPathFinder.Agents.AStarIntermediates
 {
     internal class RouteTreeNode
     {
         public GraphLink Link { get; }
-        public bool TraversingLinkReversal { get; }
+        public long DirectionalLinkID { get; }
 
-        public RouteTreeNode? ParentNode { get; }
+        public RouteTreeNode? ParentNode { get; } = null;
+        /// <summary>
+        /// Sum of all length of all ancestor links and this link
+        /// </summary>
         public double AccumulatedDistance { get; }
+
+        /// <summary>
+        /// Straight distance to destination from this link
+        /// </summary>
+        public double HeuristicDistance { get; }
 
 
         /// <summary>
@@ -16,19 +26,92 @@ namespace RoadPathFinder.Agents.AStarIntermediates
         /// </summary>
         /// <param name="link"></param>
         /// <param name="traversingLinkReversal"></param>
-        /// <param name="parentNode"></param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public RouteTreeNode(GraphLink link, bool traversingLinkReversal,
-            RouteTreeNode? parentNode)
+        /// <param name="destination"></param>
+        /// <exception cref="ArgumentNullException">link</exception>
+        public RouteTreeNode(
+            GraphLink link,
+            bool traversingLinkReversal,
+            FlatPoint destination)
         {
             ArgumentNullException.ThrowIfNull(link, nameof(link));
 
             Link = link;
-            TraversingLinkReversal = traversingLinkReversal;
 
+            AccumulatedDistance = link.Geometry.GetLength();
+
+            if (traversingLinkReversal)
+            {
+                // reversal
+                DirectionalLinkID = -link.ID;
+                HeuristicDistance = destination.GetDistance(link.StartNode);
+            }
+            else
+            {
+                // straight
+                DirectionalLinkID = link.ID;
+                HeuristicDistance = destination.GetDistance(link.EndNode);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="link"></param>
+        /// <param name="parentNode"></param>
+        /// <param name="destination"></param>
+        /// <exception cref="ArgumentException">parent node and link is not connected</exception>
+        /// <exception cref="ArgumentNullException">link, parentNode</exception>
+        public RouteTreeNode(
+            GraphLink link,
+            RouteTreeNode parentNode,
+            FlatPoint destination)
+            : this(link, !IsStraight(link, parentNode), destination)
+        {
             ParentNode = parentNode;
-            AccumulatedDistance = parentNode?.AccumulatedDistance ?? 0;
-            AccumulatedDistance += Link.Geometry.GetLength();
+            AccumulatedDistance += parentNode?.AccumulatedDistance ?? 0;
+        }
+
+
+        /// <summary>
+        /// is this link traveling straight or reversed?
+        /// </summary>
+        /// <param name="link"></param>
+        /// <param name="parentNode"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">parent node and link is not connected</exception>
+        /// <exception cref="ArgumentNullException">link, parentNode</exception>
+        private static bool IsStraight(GraphLink link, RouteTreeNode parentNode)
+        {
+            ArgumentNullException.ThrowIfNull(link, nameof(link));
+            ArgumentNullException.ThrowIfNull(parentNode, nameof(parentNode));
+
+            long parentEndNodeID = parentNode.GetDirectionalEndNodeID();
+
+            if (parentEndNodeID == link.StartNodeID)
+            {
+                return true;
+            }
+            else if (parentEndNodeID == link.EndNodeID)
+            {
+                return false;
+            }
+            else
+            {
+                throw new ArgumentException($"Link {link.ID}({link.StartNodeID}-{link.EndNodeID}) is not connected to parent link {parentNode.Link.ID}(->{parentEndNodeID})");
+            }
+        }
+
+
+        private long GetDirectionalEndNodeID()
+        {
+            if (DirectionalLinkID > 0)
+            {
+                return Link.EndNodeID;
+            }
+            else
+            {
+                return Link.StartNodeID;
+            }
         }
     }
 }
